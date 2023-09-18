0e259f2bc0a5a371d3711b1dd11cf6b39014e74e
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-17 23:14:31 -0600

    Add project files.

---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AbstractClassDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.ReadLine();
+    }
+}
+
+public abstract class InventoryItem
+{
+    public string ProductName { get; set; }
+    public int QuantityOnHand { get; set; }
+}
+
+public class Book : InventoryItem
+{
+
+}
+
+public class Vehicle : InventoryItem
+{
+
+}
